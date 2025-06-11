6234a18e4ddebb1947374a801ae02f5d6e0555e3
juktimath003 <juktimathshreya123@gmail.com>
2025-06-11 12:39:25 +0530

    HLO

---
+++ scorecalculator1.java
@@ -0,0 +1,15 @@
+public class scorecalculator1{
+    public static void main(String[] args) {
+        
+        int[] scores={ 90,80,700,85,75};
+        int sum=0;
+        for(int score : scores){
+            sum+=score;
+        }
+        double average = (double)sum/scores.length;
+        System.out.println("sum:"+average);
+        System.out.println("Average:"+ average);
+    }
+
+}
+
